a5dd870c673769670a1433d1cd00e37c58c8404c
kmschoep-usgs <kmschoep@usgs.gov>
2017-09-26 14:48:28 -0500

    add static files for agency, country, county, mcd (minor civil divisions), site type and state lookups.

--- mlrvalidator/reference.py
+++ mlrvalidator/reference.py
@@ -1,9 +1,18 @@
 import os
 import sys
+import json
 
 PROJECT_DIR = os.path.dirname(__file__)
 EXEC_PREFIX = sys.exec_prefix
 
-fd = open(os.path.join(PROJECT_DIR, 'references/agency.txt'))
-with fd:
-    agency = fd.read()
+agency_file = open(os.path.join(PROJECT_DIR, 'references/agency.txt'))
+with agency_file:
+    agency = set(line.strip() for line in agency_file)
+
+state_file = open(os.path.join(PROJECT_DIR, 'references/state.json'))
+with state_file:
+    state = json.loads(state_file.read())
+
+mcd_file = open(os.path.join(PROJECT_DIR, 'references/mcd.json'))
+with mcd_file:
+    mcd = json.loads(mcd_file.read())
