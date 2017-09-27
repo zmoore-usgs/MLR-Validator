24e569e5e95c311f81cdce2bde4cceeea9d3565f
kmschoep-usgs <kmschoep@usgs.gov>
2017-09-27 12:07:10 -0500

    add rest of lookups, consolidate "reference list"-type lookups into one json file.

--- mlrvalidator/reference.py
+++ mlrvalidator/reference.py
@@ -5,9 +5,9 @@ import json
 PROJECT_DIR = os.path.dirname(__file__)
 EXEC_PREFIX = sys.exec_prefix
 
-agency_file = open(os.path.join(PROJECT_DIR, 'references/agency.txt'))
+agency_file = open(os.path.join(PROJECT_DIR, 'references/agency.json'))
 with agency_file:
-    agency = set(line.strip() for line in agency_file)
+    agency = json.loads(agency_file.read())
 
 state_file = open(os.path.join(PROJECT_DIR, 'references/state.json'))
 with state_file:
