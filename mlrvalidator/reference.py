2d98cd9f23076b3337b721fa2b8ef7856537690c
Andrew Yan <ayan@usgs.gov>
2017-10-11 07:51:35 -0500

    first cut at site type cross field validations

--- mlrvalidator/reference.py
+++ mlrvalidator/reference.py
@@ -109,4 +109,9 @@ class SiteTypes(ReferenceInfo):
         return new_site_type_list
 
 
+class SiteTypesCrossField(ReferenceInfo):
 
+    def get_site_type_field_dependencies(self, site_type_code):
+        site_type_cross_field_refs = self.reference_info['siteTypes']
+        site_type_field_ref = next((site_type_d for site_type_d in site_type_cross_field_refs if site_type_d['siteType'] == site_type_code))
+        return site_type_field_ref
---
+++ mlrvalidator/site_type_cross_field_validator.py
@@ -0,0 +1,30 @@
+from cerberus import Validator
+
+from . import site_type_cross_field_reference
+
+
+class SiteTypeCrossFieldValidator(Validator):
+
+    def _validate_valid_site_type_cross_field(self, valid_site_type_cross_field, field, value):
+        if valid_site_type_cross_field:
+            site_type_ref = site_type_cross_field_reference.get_site_type_field_dependencies(value)
+            not_nullable_attrs = site_type_ref['notNullableAttrs']
+            nullable_attrs = site_type_ref['nullAttrs']
+            nn_attr_field_problems = []
+            n_attr_field_problems = []
+            for nn_attr in not_nullable_attrs:
+                nn_attr_field_val = self.document[nn_attr]
+                if len(nn_attr_field_val) == 0:
+                    nn_attr_field_problems.append(nn_attr)
+            for n_attr in nullable_attrs:
+                n_attr_field_val = self.document[n_attr]
+                if len(n_attr_field_val) != 0:
+                    n_attr_field_problems.append(n_attr)
+            if len(nn_attr_field_problems) > 0 or len(n_attr_field_problems) > 0:
+                nn_attrs = ', '.join(nn_attr_field_problems)
+                n_attrs = ', '.join(n_attr_field_problems)
+                error_message = ('Cross field errors for siteType {0}. '
+                                 'The following fields must not be null: {1}. '
+                                 'The following fields must be null: {2}.'
+                                 ).format(value, nn_attrs, n_attrs)
+                return self._error(field, error_message)
