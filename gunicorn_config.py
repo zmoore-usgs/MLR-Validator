9e517b5999ffe365001199a92c80bbec02738461
Andrew Yan <ayan@usgs.gov>
2017-11-03 09:36:18 -0500

    simplify keyfile and certfile definition

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -8,11 +8,7 @@ bind = '{0}:{1}'.format(bind_ip, bind_port)
 capture_output = True
 
 # ssl_key_path and ssl_cert_path environment variables are defined when secrets are created
-ssl_keyfile = os.getenv('ssl_key_path')
-ssl_certfile = os.getenv('ssl_cert_path')
-if ssl_keyfile is not None and os.path.isfile(ssl_keyfile):
-    keyfile = ssl_keyfile
-if ssl_certfile is not None and os.path.isfile(ssl_certfile):
-    certfile = ssl_certfile
+keyfile = os.getenv('ssl_key_path')
+certfile = os.getenv('ssl_cert_path')
 
 gunicorn.SERVER_SOFTWARE = ''
