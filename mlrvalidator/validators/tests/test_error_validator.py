7e57db49d2fe9eaf0547d14ba16220ffc81efc34
Carl Schroedl <cschroedl@usgs.gov>
2018-11-09 11:51:02 -0600

    Reduced repetition, increased clarity via a test util method.

--- mlrvalidator/validators/tests/test_error_validator.py
+++ mlrvalidator/validators/tests/test_error_validator.py
@@ -43,17 +43,8 @@ class ErrorValidatorErrorsTestCase(TestCase):
         self.assertEqual(len(validator.errors), 0)
 
     def test_single_field_invalid(self, mtran_class, mref_class, mcross_class, msingle_field_class):
-        mtran = mtran_class.return_value
-        mref = mref_class.return_value
-        mcross = mcross_class.return_value
+        self.setUpPassingValidators(mtran_class, mref_class, mcross_class)
         msingle_field = msingle_field_class.return_value
-
-        mtran.validate.return_value = True
-        mtran.errors = {}
-        mref.validate.return_value = True
-        mref.errors = {}
-        mcross.validate.return_value = True
-        mcross.errors = {}
         msingle_field.validate.return_value = False
         msingle_field.errors = {'A' : ['Invalid']}
 
@@ -69,19 +60,10 @@ class ErrorValidatorErrorsTestCase(TestCase):
         self.assertIn('A', validator.errors)
 
     def test_cross_field_invalid(self, mtran_class, mref_class, mcross_class, msingle_field_class):
-        mtran = mtran_class.return_value
-        mref = mref_class.return_value
+        self.setUpPassingValidators(mtran_class, mref_class, msingle_field_class)
         mcross = mcross_class.return_value
-        msingle_field = msingle_field_class.return_value
-
-        mtran.validate.return_value = True
-        mtran.errors = {}
-        mref.validate.return_value = True
-        mref.errors = {}
         mcross.validate.return_value = False
         mcross.errors = {'B': ['Invalid']}
-        msingle_field.validate.return_value = True
-        msingle_field.errors = {}
 
         validator = ErrorValidator('schema_dir', 'ref_dir', 'http://localhost')
         result = validator.validate({'A': 'This', 'B': 'That'}, {})
@@ -95,19 +77,10 @@ class ErrorValidatorErrorsTestCase(TestCase):
         self.assertIn('B', validator.errors)
 
     def test_ref_invalid(self, mtran_class, mref_class, mcross_class, msingle_field_class):
-        mtran = mtran_class.return_value
+        self.setUpPassingValidators(mtran_class, mcross_class, msingle_field_class)
         mref = mref_class.return_value
-        mcross = mcross_class.return_value
-        msingle_field = msingle_field_class.return_value
-
-        mtran.validate.return_value = True
-        mtran.errors = {}
         mref.validate.return_value = False
         mref.errors = {'B': ['Bad']}
-        mcross.validate.return_value = True
-        mcross.errors = {}
-        msingle_field.validate.return_value = True
-        msingle_field.errors = {}
 
         validator = ErrorValidator('schema_dir', 'ref_dir', 'http://localhost')
         result = validator.validate({'A': 'This', 'B': 'That'}, {})
@@ -121,19 +94,11 @@ class ErrorValidatorErrorsTestCase(TestCase):
         self.assertIn('B', validator.errors)
 
     def test_tran_invalid(self, mtran_class, mref_class, mcross_class, msingle_field_class):
+        self.setUpPassingValidators(mref_class, mcross_class, msingle_field_class)
         mtran = mtran_class.return_value
-        mref = mref_class.return_value
-        mcross = mcross_class.return_value
-        msingle_field = msingle_field_class.return_value
-
         mtran.validate.return_value = False
         mtran.errors = {'B': ['Bad transition']}
-        mref.validate.return_value = True
-        mref.errors = {}
-        mcross.validate.return_value = True
-        mcross.errors = {}
-        msingle_field.validate.return_value = True
-        msingle_field.errors = {}
+
 
         validator = ErrorValidator('schema_dir', 'ref_dir', 'http://localhost')
         result = validator.validate({'A': 'This', 'B': 'That'}, {})
@@ -173,19 +138,7 @@ class ErrorValidatorErrorsTestCase(TestCase):
         self.assertEqual(len(validator.errors.get('B')), 3)
 
     def test_duplicate_site_error(self, mtran_class, mref_class, mcross_class, msingle_field_class):
-        mtran = mtran_class.return_value
-        mref = mref_class.return_value
-        mcross = mcross_class.return_value
-        msingle_field = msingle_field_class.return_value
-
-        mtran.validate.return_value = True
-        mtran.errors = {}
-        mref.validate.return_value = True
-        mref.errors = {}
-        mcross.validate.return_value = True
-        mcross.errors = {}
-        msingle_field.validate.return_value = True
-        msingle_field.errors = {}
+        self.setUpPassingValidators(mtran_class, mref_class, mcross_class, msingle_field_class)
 
         validator = ErrorValidator('schema_dir', 'ref_dir', 'http://localhost')
         self.assertFalse(validator.validate({'agencyCode': 'USGS', 'siteNumber': '12345678'}, {'agencyCode': 'USGS', 'siteNumber': '12345678'}))
