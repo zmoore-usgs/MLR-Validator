2efb61fe21aa15eb04776974b1f5d6a67053057f
jenkins <jenkins@jenkins.wma.usgs.chs.ead>
2019-02-05 21:48:49 +0000

    Bump version: 0.12.0 → 0.13.0dev

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = read_requirements('requirements.txt')
 
 
 setup(name='usgs_wma_mlr_validator',
-      version='0.12.0',
+      version='0.13.0dev',
       description='MLR Validator Microservice',
       author='Mary Bucknell, Andrew Yan, Dave Steinich, Zack Moore, Kathy Schoephoester',
       author_email='mlr-devs@usgs.gov',
